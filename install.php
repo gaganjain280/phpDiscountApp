2fb4e064b83644ca8985b7077fe42025cefe5caa
Gagan jai <55681648+gaganjain280@users.noreply.github.com>
2020-03-20 19:33:34 +0530

    Update install.php

--- install.php
+++ install.php
@@ -1,7 +1,7 @@
 <?php
 // Set variables for our request
 $shop = $_GET['shop'];
-$api_key = "a21c30f2de950508bf4d80c2ecbbcc8f";
+$api_key = "slackcheck";
 $scopes = "read_orders,write_products,write_script_tags,read_themes,write_themes,write_marketing_events,read_marketing_events,read_customers,write_customers,read_discounts,write_discounts,read_checkouts,write_checkouts,write_price_rules ";
 $redirect_uri = "http://localhost/shopifyApp/generate_token.php";
 
